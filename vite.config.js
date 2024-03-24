7b5f4e9e9954455c12c120ce4d2ee13ebe407263
MirkaHienonen <mhienone@edu.lapinamk.fi>
2024-03-24 13:00:22 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CloudComputing_set4/",
+  base: "/dist",
   plugins: [react()],
 })
